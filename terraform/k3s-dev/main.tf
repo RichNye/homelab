edcc0e4416f662fb162b9ba38f2c9ee80c1d4bd2
RichNye <richardnye5@gmail.com>
2025-11-24 21:12:36 +0000

    Add new k3s VMs

---
+++ terraform/k3s-dev/main.tf
@@ -0,0 +1,55 @@
+module "k3s-control" {
+    count = 1
+    source = "../modules/ubuntu_vm"
+    
+    vm_name              = "k3s-control-0${count.index + 1}"
+    vm_description       = "k3s control plane"
+    vm_cores             = 1
+    vm_memory            = 2048
+    vm_tags              = "k3s,control-plane"
+    proxmox_host         = "RN-PROXMOX01"
+    ubuntu_template_name = "ubuntu-2404-cloudinit-template"
+    vm_disks = [
+        {
+        slot    = "virtio0"
+        size    = "32G"
+        storage = "data-hdd"
+        type    = "disk"
+        }
+    ]
+    vm_networks = [
+        {
+        id     = "0"
+        model  = "virtio"
+        bridge = "vmbr0"
+        }
+    ]
+}
+
+module "k3s-worker" {
+    count = 2
+    source = "../modules/ubuntu_vm"
+    
+    vm_name              = "k3s-worker-0${count.index + 1}"
+    vm_description       = "k3s worker node"
+    vm_cores             = 1
+    vm_memory            = 2048
+    vm_tags              = "k3s,worker-node"
+    proxmox_host         = "RN-PROXMOX01"
+    ubuntu_template_name = "ubuntu-2404-cloudinit-template"
+    vm_disks = [
+        {
+        slot    = "virtio0"
+        size    = "32G"
+        storage = "data-hdd"
+        type    = "disk"
+        }
+    ]
+    vm_networks = [
+        {
+        id     = "0"
+        model  = "virtio"
+        bridge = "vmbr0"
+        }
+    ]
+}
