3aa97bcd7da9c339bcfc2c14be8f40dd0958fde8
Richard Nye <richardnye5@gmail.com>
2026-02-09 19:52:23 +0000

    Create initial deploy script

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+repo_root="/home/ubuntu/gitrepos/homelab"
+playbook_path="/ansible/playbooks"
+inventory_path="/ansible/inventory"
+
+## Gather script arguments
+read -r -p "Specify the environment (prod, dev, both): "  environment
+read -r -p "Specify the server role (lb, web, db, all): " role
+read -r -p "Specify the GitHub branch name:" branch
+
+echo "Your chosen environment is $environment"
+echo "Your chosen server role is $role"
+read -r -p "Is this correct?" user_confirm
+
+case $user_confirm in
+    "n"|"N"|"no"|"No"|"NO")
+    echo "Exiting script..."
+    exit 1
+    ;;
+
+    "y"|"Y"|"yes"|"Yes"|"YES")
+    echo "Continuing script..."
+    ;;
+esac
+
+cd "$repo_root"
+
+echo "pulling latest updates from GitHub"
+git checkout "$branch"
