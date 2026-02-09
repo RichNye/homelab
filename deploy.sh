613eb0785e846ee288f702cfdc3f23783080a861
Richard Nye <richardnye5@gmail.com>
2026-02-09 20:47:41 +0000

    Add ansible playbook feature

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
 
 repo_root="/home/ubuntu/gitrepos/homelab"
-playbook_path="/ansible/playbooks"
-inventory_path="/ansible/inventory"
+playbooks_dir="ansible/playbooks"
+inventory_dir="ansible/inventory"
 
-## Gather script arguments
-read -r -p "Specify the environment (prod, dev, both): \n"  environment
-read -r -p "Specify the server role (lb, web, db, all): \n" role
-read -r -p "Specify the GitHub branch name:" branch
+read -r -p "Specify the environment (prod, dev, both): " environment
+read -r -p "Specify the server role (lb, web, db, all): " role
+read -r -p "Specify the GitHub branch name: " branch
 
 echo "Your chosen environment is $environment"
 echo "Your chosen server role is $role"
-read -r -p "Is this correct? \n" user_confirm
+read -r -p "Is this correct? (y/n): " user_confirm
 
-case $user_confirm in
-    "n"|"N"|"no"|"No"|"NO")
+case "$user_confirm" in
+  y|Y|yes|YES)
+    echo "Continuing script..."
+    ;;
+  n|N|no|NO)
     echo "Exiting script..."
     exit 1
     ;;
-
-    "y"|"Y"|"yes"|"Yes"|"YES")
-    echo "Continuing script..."
-    ;;
-
-    *)
+  *)
     echo "Incorrect input"
+    exit 1
+    ;;
 esac
 
-cd "$repo_root"
+cd "$repo_root" || exit 1
 
-echo "pulling latest updates from GitHub"
+echo "Pulling latest updates from GitHub..."
 git checkout "$branch"
+
+playbook_path="$repo_root/$playbooks_dir/$role.yaml"
+inventory_path="$repo_root/$inventory_dir/$environment/inventory.yaml"
+
+if [ ! -f "$playbook_path" ]; then
+  echo "Playbook not found. Exiting..."
+  exit 1
+fi
+
+if [ ! -f "$inventory_path" ]; then
+  echo "Inventory not found. Exiting..."
+  exit 1
+fi
+
+echo "Running playbook..."
+ansible-playbook -i "$inventory_path" "$playbook_path" --ask-vault-password --check
