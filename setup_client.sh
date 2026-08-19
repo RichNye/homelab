d87d5e3faa1d8fbe6e03eca06fb6428f92c92258
Richard Nye <richardnye5@gmail.com>
2026-08-19 21:50:08 +0100

    Remove white space

--- setup_client.sh
+++ setup_client.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # high-level steps:
 # check proxmox API env variables exist, connect to proxmox as test
-# test connection to Proxmox using ssh key
 # install terraform, initialise, output a tf plan
 # run tf apply if prompted
 
 PROXMOX_HOST="192.168.178.50"
 
 # check environment variables exist
-if [ ! $PM_API_TOKEN_SECRET ]; then
+if [ ! "$PM_API_TOKEN_SECRET" ]; then
   echo "Proxmox API secret not found, please set!"
   exit 1
 fi
-if [ ! $PM_API_TOKEN_ID ]; then
+if [ ! "$PM_API_TOKEN_ID" ]; then
   echo "Proxmox API token name not found, please set!"
   exit 1
 fi
@@ -24,9 +23,7 @@ proxmox_response=$(curl -H "Authorization: PVEAPIToken=$PM_API_TOKEN_ID=$PM_API_
 if [[ $proxmox_response != *"200 OK"* ]]; then
     echo "Proxmox API error - curl output in full:"
     echo "$proxmox_response"
+    exit 1
 else
     echo "Proxmox host tested successfully!"
 fi
-
-
-
