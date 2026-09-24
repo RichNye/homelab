f9b1642c77b8de1d0dee201ab073d08ec2f7b09c
Richard Nye <richardnye5@gmail.com>
2026-09-24 15:11:17 +0000

    Add initial Cloudflare R2 remote state block

--- terraform/production/providers.tf
+++ terraform/production/providers.tf
@@ -12,5 +12,21 @@ provider "proxmox" {
     pm_tls_insecure = true
 }
 
+backend "s3" {
+  bucket = "homelab-tfstate"
+  key = "/homelab/production.tfstate"
+  region = "auto"
+
+  # leverage AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables for secure auth
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_requesting_account_id  = true
+  skip_s3_checksum            = true
+  use_path_style              = true
+
+  endpoints = { s3 = "https://0d8b44a2f39e8ada9add507a23f368ea.r2.cloudflarestorage.com" }  
+}
 
 
